5cf3f54e185525475510c7e7a6f0bee27c63f41f
John Marshall <jm18@sanger.ac.uk>
2014-12-15 14:30:48 +0000

    Add C++ version and comment

--- test.cpp
+++ test.cpp
@@ -1,4 +1,4 @@
 const char *foo() {
-    /* \c is actually invalid; the rest are fine.  */
+    // \c is actually invalid; the rest are fine.
     return "foo \t bar \c baz \055 foo \x2D bar \n baz \0 foo";
 }
