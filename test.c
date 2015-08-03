133ebe0aa947416a6925d34ed5dcc8fdcc3cda3d
John Marshall <jm18@sanger.ac.uk>
2015-08-03 21:05:48 +0100

    Add greeting() function

--- test.c
+++ test.c
@@ -2,3 +2,7 @@ const char *foo() {
     /* \c is actually invalid; the rest are fine.  */
     return "foo \t bar \c baz \055 foo \x2D bar \n baz \0 foo";
 }
+
+const char *greeting() {
+  return "Hello, world!\n";
+}
