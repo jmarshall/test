30d6ea22960f9eee897b5e3adf40abd181a60bf1
John Marshall <jmarshall@hey.com>
2026-08-31 11:37:56 +1200

    No multiprocessing

--- multi/testmulti.py
+++ multi/testmulti.py
@@ -1,4 +1,15 @@
-import pysam
+#import pysam
 
-def test_foo(mkprocess):
-    print(mkprocess)
+import http.server
+#import multiprocessing
+#import sys
+from datetime import datetime
+
+#import pytest
+
+
+def test_foo():
+    print(f"\nS0 {datetime.now()}")
+    server = http.server.HTTPServer(("127.0.0.1", 0), http.server.SimpleHTTPRequestHandler)
+    print(f"S1 {datetime.now()}")
+    #server.serve_forever()
